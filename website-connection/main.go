d1ba9cc7213a126af1f47f5617ea9d4c2c7c31e8
Ana Luiz <acluiz.study@gmail.com>
2025-01-15 20:41:35 -0300

    feat: verify multiple urls

--- website-connection/main.go
+++ website-connection/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -29,9 +30,42 @@ func getCommand() int {
 	return command
 }
 
+func verifyWebsite(url string) {
+	resp, err := http.Get(url)
+	
+	if err != nil {
+		fmt.Printf("Ocorreu um erro, tente novamente.")
+		os.Exit(1)
+		return
+	}
+
+	if resp.StatusCode == 200  {
+		fmt.Printf("O site foi carregado com sucesso. Código de resposta: %v.\n\n", resp.StatusCode)
+	} else {
+		fmt.Printf("O site está com problemas. Código de resposta: %v.\n\n", resp.StatusCode)
+	}
+}
+
+func startMonitoring() {
+	fmt.Printf("Iniciando monitoramento...\n\n")
+
+	sites := []string{
+		"https://httpbin.org/status/200",
+		"https://httpbin.org/status/200",
+		"https://httpbin.org/status/404",
+		"https://httpbin.org/status/200",
+	}
+
+	for _, url := range(sites) {
+		fmt.Printf("Verificando a url %s...\n", url)
+		
+		verifyWebsite(url)
+	}
+}
 
 func main() {
 	welcome()
+
 	showOptions()
 
 	command := getCommand()
@@ -41,9 +75,9 @@ func main() {
 			fmt.Println("Programa encerrado.")
 			os.Exit(0)
 		case 1:
-			fmt.Println("Iniciando execução...")
+			startMonitoring()
 		case 2:
-			fmt.Println("Iniciando execução...")
+			fmt.Println("Exibindo logs...")
 		default:
 			fmt.Println("Comando não reconhecido, tente novamente.")
 			os.Exit(-1)
