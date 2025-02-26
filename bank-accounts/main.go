e0392ecce6eccc124b2234b533eedd6a90d44878
Ana Luiz <acluiz.study@gmail.com>
2025-02-25 22:28:10 -0300

    feat: add transfer method

--- bank-accounts/main.go
+++ bank-accounts/main.go
@@ -16,7 +16,8 @@ func getCommand() int {
 	fmt.Println("Choose an action:")
 	fmt.Println("1) Deposit")
 	fmt.Println("2) Withdraw")
-	fmt.Println("3) Exit")
+	fmt.Println("3) Transfer")
+	fmt.Println("4) Exit")
 
 	var command int
 	fmt.Scan(&command)
@@ -24,29 +25,17 @@ func getCommand() int {
 	return command
 }
 
-func (acc *Account) deposit() error {
-	fmt.Println("How much you want to deposit?")
-
-	var amount float64
-	fmt.Scan(&amount)
-
+func (acc *Account) deposit(amount float64) error {
 	if (amount <= 0){
 		return errors.New("invalid deposit amount")
 	}
 
 	acc.balance += amount
 
-	fmt.Printf("Deposit completed. New balance: %v.\n", acc.balance)
-
 	return nil
 }
 
-func (acc *Account) withdraw() error {
-	fmt.Println("How much you want to withdraw?")
-
-	var amount float64
-	fmt.Scan(&amount)
-
+func (acc *Account) withdraw(amount float64) error {
 	if (amount <= 0){
 		return errors.New("invalid withdrawal amount")
 	}
@@ -57,28 +46,102 @@ func (acc *Account) withdraw() error {
 	
 	acc.balance -= amount
 
-	fmt.Printf("Withdrawal completed. New balance: %v.\n", acc.balance)
+	return nil
+}
+
+func (acc *Account) transfer(amount float64, receiverAcc *Account) error {
+	if (amount <= 0){
+		return errors.New("invalid amount")
+	}
+
+	if acc.balance < amount {
+		return errors.New("insufficient balance")
+	}
+
+	acc.withdraw(amount)
+	receiverAcc.deposit(amount)
 
 	return nil
 }
 
+func execDeposit(acc Account) {
+	fmt.Println("How much you want to deposit?")
+
+	var amount float64
+	fmt.Scan(&amount)
+
+	err := acc.deposit(amount)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Deposit completed.")
+	fmt.Printf("New balance for %v: %v.\n", acc.holder_name, acc.balance)
+}
+
+func execWithdraw(acc Account) {
+	fmt.Println("How much you want to withdraw?")
+
+	var amount float64
+	fmt.Scan(&amount)
+
+	err := acc.withdraw(amount)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Withdrawal completed.")
+	fmt.Printf("New balance for %v: %v.\n", acc.holder_name, acc.balance)
+}
+
+func execTransfer(acc Account, receiverAcc *Account) {
+	fmt.Println("How much you want to transfer?")
+
+	var amount float64
+	fmt.Scan(&amount)
+
+	err := acc.transfer(amount, receiverAcc)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Transfer completed.")
+	
+	fmt.Printf("New balance for %v: %v.\n", acc.holder_name, acc.balance)
+	fmt.Printf("New balance for %v: %v.\n", receiverAcc.holder_name, receiverAcc.balance)
+}
+
 func main() {
-	acc := &Account{
+	acc := Account{
 		holder_name: "Ana",
 		branchNumber: "001",
 		accountNumber: "01234",
+		balance: 377,
+	}
+	
+	acc2 := Account{
+		holder_name: "Luiz",
+		branchNumber: "001",
+		accountNumber: "67890",
 		balance: 125.5,
 	}
 
 	command := getCommand()
 
-	if command == 1 {
-		acc.deposit()
-		return
-	}
-	
-	if command == 2 {
-		acc.withdraw()
-		return
+	switch command {
+		case 1:
+			execDeposit(acc)
+		case 2:
+			execWithdraw(acc)
+		case 3:
+			execTransfer(acc, &acc2)
+		default:
+			return
 	}
 }
