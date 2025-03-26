f9097ec37fc01d809a2861b8185a249fd5d1a2b2
Ana Luiz <acluiz.study@gmail.com>
2025-03-26 20:47:57 -0300

    feat: get dynamic content in template

--- store-manager/main.go
+++ store-manager/main.go
@@ -7,8 +7,20 @@ import (
 
 var temp = template.Must(template.ParseGlob("./templates/*.html"))
 
+type Product struct {
+	Name 				string
+	Description string
+	Price 			float64
+	Quantity 		int
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "Camiseta street 00K", Description: "Camiseta street oversize - Coleção 00K", Price: 69.9, Quantity: 22},
+		{Name: "Tênis street 00K", Description: "Tênis street all black - Coleção 00K", Price: 289.9, Quantity: 14},
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
 }
 
 func main() {
